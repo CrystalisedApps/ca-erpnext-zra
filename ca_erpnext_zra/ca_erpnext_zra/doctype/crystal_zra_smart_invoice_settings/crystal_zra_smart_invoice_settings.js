9e2fb6d3e5fd2946b3f98ed0770f6f62ca9eb37e
Marty-Muhanga <marty@crystalisedapps.com>
2025-10-16 16:23:11 +0200

    device-init-enhanced

--- ca_erpnext_zra/ca_erpnext_zra/doctype/crystal_zra_smart_invoice_settings/crystal_zra_smart_invoice_settings.js
+++ ca_erpnext_zra/ca_erpnext_zra/doctype/crystal_zra_smart_invoice_settings/crystal_zra_smart_invoice_settings.js
@@ -7,83 +7,108 @@ frappe.ui.form.on("Crystal ZRA Smart Invoice Settings", {
 		frm.add_custom_button(
 			__("Ping Server"),
 			function () {
+				frappe.dom.freeze(__("Pinging Server... Please Wait"));
 				frappe.call({
 					method: "ca_erpnext_zra.ca_erpnext_zra.apis.healthcheck.ping_server",
 					args: {
 						settings: frm.doc.name,
 					},
+					callback: function (r) {
+						frappe.dom.unfreeze();
+						if (!r.exc) {
+							// Optional: Add success message if needed
+						}
+					},
+					error: function (err) {
+						frappe.dom.unfreeze();
+					},
 				});
 			},
 			__("Smart Actions")
 		);
-// Authenticate Button
+
+		// Authenticate Button
 		frm.add_custom_button(
 			__("Authenticate"),
 			function () {
+				frappe.dom.freeze(__("Authenticating... Please Wait"));
 				frappe.call({
 					method: "ca_erpnext_zra.ca_erpnext_zra.apis.auth.authenticate",
 					args: {
 						settings_name: frm.doc.name,
 					},
 					callback: function (r) {
+						frappe.dom.unfreeze();
 						if (!r.exc) {
 							frappe.msgprint(__("Authentication successful. Token updated."));
 							frm.reload_doc();
 						}
 					},
+					error: function (err) {
+						frappe.dom.unfreeze();
+					},
 				});
 			},
 			__("Smart Actions")
 		);
+
+		// Get Codes Button
 		frm.add_custom_button(
-  __("Get Codes"),
-  function () {
-    frappe.call({
-         method:
-        "ca_erpnext_zra.ca_erpnext_zra.background_tasks.tasks.refresh_vsdc_codes",
-      args: {
-        settings_name: frm.doc.name,
-        LastReqDt: frm.doc.LastReqDt || "", 
-      },
-	callback: () => {
+			__("Get Codes"),
+			function () {
+				frappe.dom.freeze(__("Refreshing Codes and Item Classifications... Please Wait"));
+				frappe.call({
+					method: "ca_erpnext_zra.ca_erpnext_zra.background_tasks.tasks.refresh_vsdc_codes",
+					args: {
+						settings_name: frm.doc.name,
+						LastReqDt: frm.doc.LastReqDt || "",
+					},
+					callback: () => {
 						frappe.call({
-							method:
-								"ca_erpnext_zra.ca_erpnext_zra.background_tasks.tasks.get_item_classification_codes",
+							method: "ca_erpnext_zra.ca_erpnext_zra.background_tasks.tasks.get_item_classification_codes",
 							args: {
 								settings_name: frm.doc.name,
-								
-   
-    lastReqDt: "20231215000000"
-
+								lastReqDt: "20231215000000",
 							},
 							callback: (r) => {
-								 console.log("Raw API response:", r); 
-								frappe.msgprint(__("Codes and Item Classifications refreshed successfully."));
+								frappe.dom.unfreeze();
+								console.log("Raw API response:", r);
+								frappe.msgprint(
+									__("Codes and Item Classifications refreshed successfully.")
+								);
+							},
+							error: function (err) {
+								frappe.dom.unfreeze();
 							},
 						});
 					},
-      error: (error) => {
-        // Error handling deferred to the server
-      },
-    });
-  },
-  __("Smart Actions")
-);
+					error: function (err) {
+						frappe.dom.unfreeze();
+					},
+				});
+			},
+			__("Smart Actions")
+		);
 
-		//Initialize Device Button
+		// Initialize Device Button
 		frm.add_custom_button(
 			__("Initialize Device"),
 			function () {
+				frappe.dom.freeze(__("Initializing Device... Please Wait"));
 				frappe.call({
 					method: "ca_erpnext_zra.ca_erpnext_zra.apis.device.initialize_device",
 					args: {
 						settings_name: frm.doc.name,
 					},
 					callback: function (r) {
+						frappe.dom.unfreeze();
 						if (!r.exc) {
 							frappe.msgprint(__("Device initialization successful."));
 							frm.reload_doc();
 						}
+					},
+					error: function (err) {
+						frappe.dom.unfreeze();
 					},
 				});
 			},
